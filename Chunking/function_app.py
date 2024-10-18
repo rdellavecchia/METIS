c3cda8267404e153ed538670149d86311aaf541b
rdellavecchia <raf318050@gmail.com>
2024-10-18 17:18:04 +0200

    Chunking v2.8

--- Chunking/function_app.py
+++ Chunking/function_app.py
@@ -5,15 +5,31 @@ import json
 import redis
 import spacy
 import fitz # PyMuPDF
+import numpy as np
 from time import time
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Configurazione per connettersi a un'istanza di Azure Cache for Redis
 REDIS_HOST = os.getenv('REDIS_HOST', 'metis.redis.cache.windows.net')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6380))  # Porta SSL
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', 'S1DHsgrmOCZSCaGw5tW9Yh01bg64v9g7YAzCaFEbFsA=')  # Primary Key
 
-# Configurazione del logger di Windows Server
+# Caricamento del sentencizer per l'italiano
+nlp_it = spacy.blank('it')
+nlp_it.add_pipe("sentencizer")
+nlp_it.max_length = 2000000
+
+# Caricamento del sentencizer per l'inglese
+nlp_en = spacy.blank('en')
+nlp_en.add_pipe("sentencizer")
+nlp_en.max_length = 2000000
+
+# Caricamento del modello di SentenceTransformer
+embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
 def configure_logger():
+    """Configurazione del logger di sistema."""
     logger = logging.getLogger("Chunking")
     if not logger.handlers:  # Evita duplicati
         logger.setLevel(logging.INFO)
@@ -40,163 +56,152 @@ def configure_logger():
 
     return logger
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
 # Inizializzazione del logger
 logger = configure_logger()
 
-# Caricamento del sentencizer per l'italiano
-nlp_it = spacy.blank('it')
-nlp_it.add_pipe("sentencizer")
-nlp_it.max_length = 2000000
-# Caricamento del sentencizer per l'inglese
-nlp_en = spacy.blank('en')
-nlp_en.add_pipe("sentencizer")
-nlp_en.max_length = 2000000
+def extract_sentences_with_indices(text, nlp_model):
+    """Estrazione delle frasi con indice da un blocco di testo mediante il sentencizer spaCy."""
+    doc = nlp_model(text)
+    sentences_with_indices = []
+    for i, sentence in enumerate(doc.sents):
+        sentences_with_indices.append((i, sentence.text))
+    return sentences_with_indices
 
-def extract_sentences_from_text(text, nlp_model):
-    """Estrazione delle frasi da un blocco di testo usando spaCy."""
-    doc = nlp_model(text)   # Elaborazione del testo con il sentencizer spaCy
-    return [sent.text for sent in doc.sents]  # Ritorno delle frasi processate
-
-def create_text_units(sentences, unit_size=3):
-    """Creazione delle unità di testo costituite da `unit_size` frasi contigue."""
+def create_text_units_with_indices(sentences_with_indices, unit_size=3):
+    """Creazione delle unità di testo con indice e associazione di `unit_size` frasi contigue."""
     units = []
     buffer = []
-    
-    for sentence in sentences:
-        buffer.append(sentence)
+    for index, sentence in sentences_with_indices:
+        buffer.append((index, sentence))
         if len(buffer) >= unit_size:
-            unit = " ".join(buffer[:unit_size]) # Creazione dell'unità unendo le prime `unit_size` frasi del buffer
-            units.append(unit)
-            buffer.pop(0) # Rimozione della prima frase dal buffet (shift)
-    
+            unit = " ".join([s for _, s in buffer[:unit_size]])
+            indices = [idx for idx, _ in buffer[:unit_size]]
+            units.append({"text": unit, "indices": indices})
+            buffer.pop(0)
     return units
 
 def extract_text_from_pdf(pdf_path):
-    """Estrazione del testo da un PDF usando PyMuPDF."""
+    """Estrazione del testo da un PDF mediante PyMuPDF."""
     try:
-        #Apertura del PDF con PyMuPDF
         with fitz.open(pdf_path) as pdf_document:
             text = ""
             for page_num in range(len(pdf_document)):
-                page = pdf_document.load_page(page_num) # Caricamento della pagina corrente
-                text += page.get_text()  # Estrazione del testo
+                page = pdf_document.load_page(page_num) 
+                text += page.get_text()
         return text
     except Exception as e:
-        logger.error(f"Errore nell'estrazione del testo dal PDF {pdf_path}: {e}")
+        logger.error(f"Errore durante l'estrazione del testo dal PDF {os.path.basename(pdf_path)}: {e}")
         return ""
+
+def generate_embeddings(units, model):
+    """Generazione (e associazione) degli embeddings per ciascuna unità (a ciascuna unità)."""
+    texts = [unit['text'] for unit in units]
+    embeddings = model.encode(texts, convert_to_tensor=True)
+    for i, unit in enumerate(units):
+        unit["combined_sentence_embedding"] = embeddings[i].tolist()
+    return units
+
+def calculate_distance(units):
+    """Calcolo delle distanze tra embeddings consecutivi mediante la similarità del coseno"""
+    distances = []
+    
+    for i in range(len(units) - 1):
+        embedding_current = units[i]["combined_sentence_embedding"]
+        embedding_next = units[i + 1]["combined_sentence_embedding"]
         
+        similarity = cosine_similarity([embedding_current], [embedding_next])[0][0]
+        distance = 1 - similarity
+        distances.append(distance)
+        
+        units[i]["distance_to_next"] = distance
+
+    return distances, units
+
+def create_chunks_based_on_distances(units, distances, pdf_file, logger):
+    """Creazione dei chunk sualla base della distanza e del 95° percentile."""
+    breakpoint = np.percentile(distances, 95) # Calcolo del 95° percentile per determinel il breakpoint
+    indices_above_threshold = [i for i, d in enumerate(distances) if d > breakpoint] # Ricerca degli indici in cui le distanze sono al di sopra della soglia
+    logger.info(f"Trovati {len(indices_above_threshold)} chunk per {pdf_file}")
+    
+    chunks = []
+    start_index = 0
+    for x in indices_above_threshold:
+        end_index = x  # Fine del chunk
+        group = units[start_index:end_index + 1] # Raccolta delle frasi tra start_index ed end_index
+        combined_text = ' '.join([d["text"] for d in group]) # Unione del testo del chunk
+        chunks.append(combined_text.strip()) # Inserimento del chunk nella lista
+        start_index = x + 1 # Aggiornamento dello start_index per il prossimo chunk
+    
+    if start_index < len(units):
+        combined_text = ' '.join([d["text"] for d in units[start_index:]])
+        chunks.append(combined_text.strip()) # Aggiunta dell'ultimo chunk nel momento in cui ci sono frasi rimanenti
+    
+    logger.info(f"Numero totale di chunk creati: {len(chunks)}")
+    
+    return chunks
+
 def process_single_pdf(pdf_path, nlp_model):
     """Elaborazione del PDF e rilascio della memoria non appena completata."""
-    logger.info(f"Estrazione del contenuto dal PDF: {os.path.basename(pdf_path)}")
-    
-    # Estrazione del testo dal PDF e restituzione di tutte le unità
-    text = extract_text_from_pdf(pdf_path)
-    
+    text = extract_text_from_pdf(pdf_path) # Estrazione del testo dal PDF e restituzione di tutte le unità
     if not text:
         logger.error(f"Nessun testo trovato nel PDF {pdf_path}")
         return[]
     
-    # Segmentazione del testo in frasi con spaCy
-    sentences = extract_sentences_from_text(text, nlp_model)
-    
-    # Liberazione della memoria utilizzata dal testo appena processato
-    del text
-    
-    # Crazione delle unità di testo
-    all_units = create_text_units(sentences)
-    
-    # Liberazione della memoria utilizzata dalle frasi una volta create le unità di testo
-    del sentences
+    sentences = extract_sentences_with_indices(text, nlp_model) # Segmentazione del testo in frasi con spaCy
+    del text # Liberazione della memoria utilizzata dal testo appena processato
+    all_units = create_text_units_with_indices(sentences) # Crazione delle unità di testo
+    del sentences # Liberazione della memoria utilizzata dalle frasi una volta create le unità di testo
     
     return all_units
 
 def process_units(pdf_path, client, nlp_model, logger):
-    '''Elaborazione di un singolo PDF, segmentazione del testo in frasi, salvataggio del risultato su Redis.'''
+    """Elaborazione di un singolo PDF, generazione degli embeddings e creazione dei chunk."""
     try:
         cached_result = client.get(pdf_path)
         if cached_result:
             logger.info(f"Risultato recuperato da Redis per {pdf_path}")
             return f"Recuperato da cache: {pdf_path}", json.loads(cached_result)
         
-        units = retry(process_single_pdf, retries=3, delay=2, pdf_path=pdf_path, nlp_model=nlp_model) # Estrazione delle frasi    
-        client.set(pdf_path, str(units)) # Caching dei risultati in Redis
-        return f"Elaborazione completata per il PDF: {pdf_path}", units
+        # Estrazione delle frasi dal PDF e generazione delle unità
+        units = process_single_pdf(pdf_path, nlp_model)
+        
+        # Generazione degli embeddings
+        units_with_embeddings = generate_embeddings(units, embedding_model)
+        
+        # Calcolo delle distanze tra gli embeddings consecutivi
+        distances, units_with_distances = calculate_distance(units_with_embeddings)
+        
+        # Creazione dei chunk sulla base delle distanze ottenute
+        chunks = create_chunks_based_on_distances(units_with_distances, distances, pdf_path, logger)
+        
+        # Caching dei chunk in Redis: `pdf_path` utilizzato come chiave per accedere ai chunk; `json.dumps(chunks)` converte la lista di chunk in una stringa JSON 
+        client.set(pdf_path, json.dumps(chunks))
+        
+        return f"Elaborazione completata per il PDF: {pdf_path}", chunks
     
     except Exception as e:
         logger.error(f"Errore nell'elaborazione del PDF {pdf_path}: {e}")
         return f"Errore per {pdf_path}: {e}", []
 
-def save_all_units_to_single_json(all_units, output_filename):
-    """Salva tutte le unità estratte da più PDF in un singolo file JSON."""
-    try:
-        # Creazione della directory per il file JSON, se non esiste
-        os.makedirs('./documentation_units', exist_ok=True)
-        
-        # Path del file JSON
-        json_filepath = os.path.join('./documentation_units', output_filename)
-        
-        # Struttura delle unità con numero e testo
-        unit_data = [{"numero_unità": i + 1, "testo_unità": unit} for i, unit in enumerate(all_units)]
-        
-        # Salvataggio delle unità in un file JSON
-        with open(json_filepath, 'w', encoding='utf-8') as json_file:
-            json.dump(unit_data, json_file, ensure_ascii=False, indent=4)
-        
-        logger.info(f"File JSON unico salvato: {json_filepath}")
-    
-    except Exception as e:
-        logger.error(f"Errore nel salvataggio del file JSON unico: {e}")
-
 def process_documentation(pdf_files, client, nlp_model, logger, doc_name):
-    '''Funzione per processare la documentazione passata come parametro.'''
-    logger.info(f"Avvio dell'elaborazione della documentazione di {doc_name} ...")
+    """Funzione per processare la documentazione passata come parametro."""
     start_time = time()  # Inizio del timer
-
-    results = [] # Per memorizzare i risultati di elaborazione
-    messages = []  # Per memorizzare i messaggi di elaborazione
-    all_units = []  # Per memorizzare tutte le unità generate dai PDF
+    all_chunks = []
 
     for pdf in pdf_files:
         try:
-            result, units = process_units(pdf, client, nlp_model, logger)
-            results.append((result, units))
-            messages.append(result)
-            all_units.extend(units)
+            chunks = process_units(pdf, client, nlp_model, logger)
+            all_chunks.extend(chunks)
         except redis.ConnectionError as conn_err:
             logger.error(f"Errore di connessione a Redis per il PDF {pdf}: {conn_err}")
         except fitz.FileDataError as file_err:
             logger.error(f"Errore durante la lettura del PDF {pdf}: {file_err}")
         except Exception as e:
             logger.error(f"Errore sconosciuto durante l'elaborazione del PDF {pdf}: {e}")
+    
+    logger.info(f"Elaborazione completata per {doc_name} in {time() - start_time} secondi")
 
-    try:
-        # Salvataggio di tutte le unità in un unico file JSON
-        save_all_units_to_single_json(all_units, f'all_units_{doc_name.lower()}.json')
-    except IOError as io_err:
-        logger.error(f"Errore nel salvataggio del file JSON per {doc_name}: {io_err}")
-    except Exception as e:
-        logger.error(f"Errore sconosciuto nel salvataggio delle unità di {doc_name}: {e}")    
-
-    end_time = time()  # Fine del timer
-    total_time = end_time - start_time
-
-    logger.info(f"Tempo totale di esecuzione per l'elaborazione della documentazione di {doc_name}: {total_time} secondi")
-    logger.info(f"Fine dell'elaborazione della documentazione di {doc_name}")
-
-def retry(func, retries=3, delay=2, *args, **kwargs):
-    """Funzione di utilità per tentare nuovamente una funzione in caso di errore."""
-    for attempt in range(retries):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            if attempt < retries - 1:
-                logger.warning(f"Tentativo {attempt + 1} fallito: {e}. Riprovo tra {delay} secondi...")
-                time.sleep(delay)
-            else:
-                logger.error(f"Fallimento dopo {retries} tentativi: {e}")
-                raise
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger_chunking")
 def http_trigger_chunking(req: func.HttpRequest) -> func.HttpResponse:
@@ -215,26 +220,11 @@ def http_trigger_chunking(req: func.HttpRequest) -> func.HttpResponse:
     directory_ws_path = r"C:\Users\rdell\OneDrive - Politecnico di Torino\Desktop\Reply9\METIS\Scraping\WindowsServer\documentsWinServer"
     
     # Lista dei PDF Red Hat 8
-    pdf_relh8_files = []
+    pdf_relh8_files = [os.path.join(directory_relh8_path, f) for f in os.listdir(directory_relh8_path) if f.endswith('.pdf')]
     # Lista dei PDF Red Hat 9
-    pdf_relh9_files = []
+    pdf_relh9_files = [os.path.join(directory_relh9_path, f) for f in os.listdir(directory_relh9_path) if f.endswith('.pdf')]
     # Lista dei PDF Windows Server
-    pdf_ws_files = []
-    
-    # Scansione della documentazione di Red Hat 8
-    for filename in os.listdir(directory_relh8_path):
-        if filename.endswith('.pdf'):
-            pdf_relh8_files.append(os.path.join(directory_relh8_path, filename))
-    
-    # Scansione della documentazione di Red Hat 9
-    for filename in os.listdir(directory_relh9_path):
-        if filename.endswith('.pdf'):
-            pdf_relh9_files.append(os.path.join(directory_relh9_path, filename))
-    
-    # Scansione della documentazione di Windows Server
-    for filename in os.listdir(directory_ws_path):
-        if filename.endswith('.pdf'):
-            pdf_ws_files.append(os.path.join(directory_ws_path, filename))
+    pdf_ws_files = [os.path.join(directory_ws_path, f) for f in os.listdir(directory_ws_path) if f.endswith('.pdf')]
     
     # Elaborazione di ciascun PDF della documentazione Red Hat 8
     process_documentation(pdf_relh8_files, client, nlp_en, logger, "Red Hat 8")
